9853fc025aaec1c006aee831a12f95ab6ce2bd09
wangyijin209 <7801046334@qq.com>
2023-06-29 22:50:45 +0800

    create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter as OnebotV11Adapter  # 避免重复命名
+
+# 初始化 NoneBot
+nonebot.init()
+
+# 注册适配器
+driver = nonebot.get_driver()
+driver.register_adapter(OnebotV11Adapter)
+
+# 在这里加载插件
+nonebot.load_builtin_plugins("echo")  # 内置插件
+# nonebot.load_plugin("thirdparty_plugin")  # 第三方插件
+# nonebot.load_plugins("awesome_bot/plugins")  # 本地插件
+
+if __name__ == "__main__":
+        nonebot.run()
